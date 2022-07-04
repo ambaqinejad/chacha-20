16433a5cd69bb7e1d7413ab7f150d8bd4df3ff43
Amir Hosein Baqinejad <ambaqinejad@gmail.com>
2022-07-04 10:46:47 +0430

    chacha20 encryption implementation was completed

--- ChaCha20.cpp
+++ ChaCha20.cpp
@@ -8,3 +8,14 @@ ChaCha20::ChaCha20(const uint8_t *key, const uint8_t *nonce, uint64_t counter) :
                    block{key, nonce}, position{64} {
     block.setCounter(counter);
 }
+
+void ChaCha20::crypt(uint8_t *bytes, size_t n_bytes) {
+    for(size_t i = 0; i < n_bytes; i++) {
+        if (position >= 64) {
+            block.next(keyStream8);
+            position = 0;
+        }
+        bytes[i] ^= keyStream8[position];
+        position ++;
+    }
+}
